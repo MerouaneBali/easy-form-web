9c4736cf9552ac2280995cd954a6be8d57a9bf7b
Merouane Bali <merouane.bali.inbox@gmail.com>
2022-02-26 20:24:55 +0100

    feat: api utility module, powered by axios

---
+++ src/utils/api.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+class Api {
+  constructor() {
+    this.instance = axios.create({
+      baseURL: process.env.NEXT_PUBLIC_API_HOST,
+      withCredentials: true,
+    });
+  }
+
+  async isUserLoggedIn() {
+    try {
+      const res = await this.instance.get("/authentication");
+      console.log(res);
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+}
+
+export default new Api();
