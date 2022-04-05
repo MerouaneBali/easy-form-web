28c0e25522bceb760f6e0b38bc283619c4310d58
Merouane Bali <merouane.bali.inbox@gmail.com>
2022-04-05 15:09:10 +0100

    fix: corrected imports order

--- src/components/MultistepOnboarding/index.jsx
+++ src/components/MultistepOnboarding/index.jsx
@@ -7,9 +7,9 @@ import Step1 from "./Step1";
 import Step2 from "./Step2";
 import Step3 from "./Step3";
 import Step4 from "./Step4";
+import Step5 from "./Step5";
 
 import "swiper/css";
-import Step5 from "./Step5";
 
 function MultistepOnboarding() {
   const [swiper, setSwiper] = useState();
